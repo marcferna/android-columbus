0a7eae34d8df1be7ac6e0b4b6e1c6046bfead6f2
Marc Fernandez <marc.fernandezg@gmail.com>
2014-10-09 23:06:16 -0700

    Create tabs with swipe functionality. #15

--- app/src/main/java/com/codepath/columbus/columbus/fragments/exhibit/ExhibitContentFragment.java
+++ app/src/main/java/com/codepath/columbus/columbus/fragments/exhibit/ExhibitContentFragment.java
@@ -3,7 +3,11 @@ package com.codepath.columbus.columbus.fragments.exhibit;
 
 
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,17 +20,51 @@ import com.codepath.columbus.columbus.R;
  */
 public class ExhibitContentFragment extends Fragment {
 
+  static final int NUM_ITEMS = 10;
+
+  private FragmentActivity activity;
+  private ViewPager viewPager;
 
   public ExhibitContentFragment() {
       // Required empty public constructor
   }
 
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    activity = getActivity();
+  }
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
       // Inflate the layout for this fragment
-      return inflater.inflate(R.layout.fragment_exhibit_content, container, false);
+    View v = inflater.inflate(R.layout.fragment_exhibit_content, container, false);
+    // ViewPager and its adapters use support library
+    // fragments, so use getSupportFragmentManager.
+    mDemoCollectionPagerAdapter =
+        new ExhibitContentPagerAdapter(activity.getSupportFragmentManager());
+    viewPager = (ViewPager) v.findViewById(R.id.vpExhibitContent);
+    viewPager.setAdapter(ExhibitContentPagerAdapter);
+  }
+
+
+  // Since this is an object collection, use a FragmentStatePagerAdapter,
+  // and NOT a FragmentPagerAdapter.
+  public class ExhibitContentPagerAdapter extends FragmentPagerAdapter {
+    public ExhibitContentPagerAdapter(FragmentManager fm) {
+      super(fm);
+    }
+
+    @Override
+    public int getCount() {
+      return NUM_ITEMS;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+      return ArrayListFragment.newInstance(position);
+    }
   }
 
 
--- app/src/main/java/com/codepath/columbus/columbus/fragments/exhibit/ExhibitHeaderFragment.java
+++ app/src/main/java/com/codepath/columbus/columbus/fragments/exhibit/ExhibitHeaderFragment.java
@@ -1,12 +1,10 @@
 package com.codepath.columbus.columbus.fragments.exhibit;
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
