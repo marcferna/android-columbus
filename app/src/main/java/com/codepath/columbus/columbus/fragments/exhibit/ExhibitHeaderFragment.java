a86fe846cbe106738df5e5086e760d9bcc521712
Marc Fernandez <marc.fernandezg@gmail.com>
2014-10-21 23:17:49 -0700

    Eliminate the image carousel page indicator and scale images.

--- app/src/main/java/com/codepath/columbus/columbus/fragments/exhibit/ExhibitHeaderFragment.java
+++ app/src/main/java/com/codepath/columbus/columbus/fragments/exhibit/ExhibitHeaderFragment.java
@@ -2,7 +2,6 @@ package com.codepath.columbus.columbus.fragments.exhibit;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,9 +10,6 @@ import android.view.ViewGroup;
 import com.codepath.columbus.columbus.R;
 import com.codepath.columbus.columbus.adapters.ImageSlideAdapter;
 import com.codepath.columbus.columbus.models.Exhibit;
-import com.codepath.columbus.columbus.utils.CirclePageIndicator;
-import com.codepath.columbus.columbus.utils.PageIndicator;
-
 import java.util.List;
 
 /**
@@ -24,7 +20,6 @@ public class ExhibitHeaderFragment extends ExhibitFragment {
 
   // UI References
   private ViewPager viewPager;
-  private PageIndicator pageIndicator;
 
   public static ExhibitHeaderFragment newInstance(Exhibit exhibit) {
     ExhibitHeaderFragment fragment = new ExhibitHeaderFragment();
@@ -55,11 +50,6 @@ public class ExhibitHeaderFragment extends ExhibitFragment {
     if (images != null && images.size() > 0) {
       viewPager = (ViewPager) view.findViewById(R.id.view_pager);
       viewPager.setAdapter(new ImageSlideAdapter(activity, images));
-    }
-
-    if (images != null && images.size() > 1) {
-      pageIndicator = (CirclePageIndicator) view.findViewById(R.id.indicator);
-      pageIndicator.setViewPager(viewPager, 0);
     }
   }
 }
