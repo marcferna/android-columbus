ea8a973eb269edfa6d54e29a1372ab61c6475d50
Priyanka <piyu19@gmail.com>
2014-10-25 16:07:41 -0700

    Add search option to exhibit list.

--- app/src/main/java/com/codepath/columbus/columbus/activities/ExhibitListActivity.java
+++ app/src/main/java/com/codepath/columbus/columbus/activities/ExhibitListActivity.java
@@ -2,23 +2,32 @@ package com.codepath.columbus.columbus.activities;
 
 import android.app.ActionBar;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.text.Html;
 import android.util.Log;
-import android.view.MenuItem;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+import com.actionbarsherlock.view.MenuInflater;
+import android.widget.SearchView;
+import android.widget.SearchView.OnQueryTextListener;
 
 import com.codepath.columbus.columbus.R;
 import com.codepath.columbus.columbus.fragments.exhibit_list.ExhibitListFragment;
+import com.codepath.columbus.columbus.fragments.exhibit_list.ExhibitListSearchFragment;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 
-public class ExhibitListActivity extends FragmentActivity {
+public class ExhibitListActivity extends SherlockFragmentActivity {
     private ExhibitListFragment exhibitListFragment;
 
     private String museumNickname;
+    private String museumId;
 
     @Override
     protected void attachBaseContext(Context newBase) {
@@ -30,10 +39,9 @@ public class ExhibitListActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exhibit_list);
-        String museumId = getIntent().getStringExtra("museumId");
+        museumId = getIntent().getStringExtra("museumId");
         String museumUUID = getIntent().getStringExtra("museumUUID");
         museumNickname = getIntent().getStringExtra("museumNickname");
-        //museumId = "zguAHcyS7S";
         //museumUUID = "8492e75f-4fd6-469d-b132-043fe94921d8";
         Log.i("INFO", "activity museum id=" + museumId + "; uuid=" + museumUUID);
 
@@ -59,6 +67,39 @@ public class ExhibitListActivity extends FragmentActivity {
         FragmentTransaction ft = fragmentManager.beginTransaction();
         ft.replace(R.id.flListContainer, exhibitListFragment);
         ft.commit();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getSupportMenuInflater();
+        inflater.inflate(R.menu.exhibit_list, menu);
+        MenuItem searchItem = menu.findItem(R.id.menu_search_exhibits);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setQueryHint("Search");
+        searchView.setOnQueryTextListener(new OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                searchExhibits(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    public void searchExhibits(String query) {
+        Intent i = new Intent(ExhibitListActivity.this, SearchActivity.class);
+        i.putExtra("museumId", museumId);
+        i.putExtra("queryString", query);
+        i.putExtra("museumNickname", museumNickname);
+        startActivity(i);
+
     }
 
     @Override
--- app/src/main/java/com/codepath/columbus/columbus/fragments/exhibit_list/ExhibitListFragment.java
+++ app/src/main/java/com/codepath/columbus/columbus/fragments/exhibit_list/ExhibitListFragment.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.RemoteException;
-import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +15,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.actionbarsherlock.app.SherlockFragment;
 import com.codepath.columbus.columbus.R;
 import com.codepath.columbus.columbus.activities.ExhibitActivity;
 import com.codepath.columbus.columbus.adapters.ExhibitListAdapter;
@@ -41,7 +41,7 @@ import uk.co.senab.actionbarpulltorefresh.extras.actionbarsherlock.PullToRefresh
 import uk.co.senab.actionbarpulltorefresh.library.ActionBarPullToRefresh;
 import uk.co.senab.actionbarpulltorefresh.library.listeners.OnRefreshListener;
 
-public class ExhibitListFragment extends Fragment {
+public class ExhibitListFragment extends SherlockFragment {
     private PtrStickyListHeadersListView lvExhibitList;
     private ArrayList<Exhibit> exhibits;
     private ArrayAdapter<Exhibit> aExhibits;
@@ -257,9 +257,11 @@ public class ExhibitListFragment extends Fragment {
 
     private Exhibit findExhibitByBeaconId(String beaconID) {
         for(Exhibit exhibit: exhibits) {
-            Log.i("INFO", "comparing with " + exhibit.getBeaconId());
-            if(exhibit.getBeaconId().equalsIgnoreCase(beaconID)) {
-                return exhibit;
+            if(exhibit.getBeaconId() != null) {
+                Log.i("INFO", "comparing with " + exhibit.getBeaconId());
+                if (exhibit.getBeaconId().equalsIgnoreCase(beaconID)) {
+                    return exhibit;
+                }
             }
         }
         return null;
